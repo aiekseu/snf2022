f016355277a55755ee1d3039de01efc8e6ee5ff7
Simraki <Simraki@mail.ru>
2022-01-22 20:45:34 +0700

    experimental fix for github pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 const App = () => {
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
             <Routes>
-                <Route exact path="/" element={<CountdownPage/>}/>
+                <Route exact path="" element={<CountdownPage/>}/>
                 <Route path="/stream" element={<MainPage/>}/>
                 <Route path="/test/:num" element={<TestPage/>}/>
             </Routes>
