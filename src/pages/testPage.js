836de6fcc10cd14a0b75ed62606ff413059d5984
Simraki <Simraki@mail.ru>
2022-01-27 16:05:45 +0700

    new test page \ update questions

--- src/pages/testPage.js
+++ src/pages/testPage.js
@@ -91,6 +91,7 @@ const TestPage = () => {
         }
     });
     theme = responsiveFontSizes(theme);
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
     const {num} = useParams();
 
@@ -106,9 +107,10 @@ const TestPage = () => {
             <Box pb={4}
                  style={{
                      minHeight: '100vh',
+                     height: '100%',
                      backgroundImage: `url(${bg})`,
                      backgroundPosition: 'center top',
-                     backgroundRepeat: 'repeat-x',
+                     backgroundRepeat: 'repeat',
                  }}
             >
                 <Box textAlign={'center'}>
@@ -122,41 +124,53 @@ const TestPage = () => {
                                   }}
                     />
                 </Box>
-                <Question
-                    num={num}
-                    question={questions[num - 1].question}
-                    answers={questions[num - 1].answers}
-                />
+                {
+                    isMobile ?
+                        <MobileQuestion
+                            num={num}
+                            question={questions[num - 1]}
+                        />
+                        :
+                        <Question
+                            num={num}
+                            question={questions[num - 1]}
+                        />
+                }
             </Box>
         </ThemeProvider>
     );
 }
 
+const maxNum = 5;
+const isLastQuestion = (num) => num === maxNum;
+const isWideQuestion = (num) => num === 3;
 
-const Question = (props) => {
+const sendData = (id) => {
+    if (id < 0) {
+        return
+    }
+    console.log(`Send >>> [${id}]`);
+    Axios.get(`https://sanofi-genzyme.herokuapp.com/api/answers/${id}/vote`)
+        .subscribe((res) => console.log(res.data));
+};
+
+const MobileQuestion = ({num, question}) => {
+
     const [answerId, setAnswerId] = useState(-1);
     const [hasError, setHasError] = useState(false);
 
-    let num = parseInt(props.num);
-    const maxNum = 5;
+    num = parseInt(num);
 
     let navigate = useNavigate();
-    const isMobile = useMediaQuery((theme) => theme.breakpoints.down('md'));
-
-    const sendData = (id) => {
-        console.log(`Send >>> [${id}]`);
-        Axios.get(`https://sanofi-genzyme.herokuapp.com/api/answers/${id}/vote`)
-            .subscribe((res) => console.log(res.data));
-    };
 
     const goNext = () => {
-        if (answerId === -1) {
+        if (answerId === -1 && !isLastQuestion(num)) {
             setHasError(true);
             return;
         }
         sendData(answerId);
         setAnswerId(-1);
-        if (num === maxNum) {
+        if (isLastQuestion(num)) {
             console.log('FINISH');
             navigate('/result', {replace: true});
         } else {
@@ -172,18 +186,19 @@ const Question = (props) => {
 
     return (
         <Container maxWidth='md' sx={{mt: 1}}>
-            <Grid container justifyContent={'space-between'} alignItems={'center'}>
+            <Grid container justifyContent={'center'} alignItems={'center'}>
+                <Grid item xs={10}>
+                    <hr style={{border: '1.1px solid #C50099', background: '#C50099'}}/>
+                </Grid>
                 <Grid item xs={2}>
-                    <Typography variant={'h3'} fontWeight={500}>
+                    <Typography variant={'h3'} fontWeight={500} align={'right'} fontFamily={'digital-clock-font'}>
                         {num}/{maxNum}
                     </Typography>
                 </Grid>
-                <Grid item xs={10}>
-                    <Typography variant={isMobile ? 'h6' : 'h4'} align={'center'}>
-                        {props.question}
-                    </Typography>
-                </Grid>
             </Grid>
+            <Typography variant={'h6'} align={'justify'} whiteSpace={'pre-line'}>
+                {question.legend}
+            </Typography>
             <Stack mt={2} justifyContent={'space-between'} alignItems={'stretch'}>
                 <img
                     src={city}
@@ -192,19 +207,22 @@ const Question = (props) => {
                         height: 'auto',
                         boxSizing: 'border-box',
                         width: '100%',
-                        borderWidth: 8,
+                        borderWidth: 4,
                         borderStyle: 'solid',
                         borderImage: 'linear-gradient(180deg, rgba(244,64,148,1) 0%, rgba(85,74,218,1) 100%)',
                         borderImageSlice: 1,
                     }}/>
+                <Typography variant={'h6'} fontWeight={500} align={'center'} mt={2}>
+                    {question.question}
+                </Typography>
                 <RadioGroup
-                    defaultValue={''}
+                    defaultValue={-1}
                     onChange={handleChange}
                 >
                     <Stack mt={1} spacing={1}>
                         <div/>
                         {
-                            props.answers.map((ans) => {
+                            question.answers?.map((ans) => {
                                 return ans.id === answerId ?
                                     <SelectedRadioControlLabel value={ans.id} label={ans.text} key={ans.id}/>
                                     :
@@ -213,20 +231,118 @@ const Question = (props) => {
                         }
                     </Stack>
                 </RadioGroup>
-                <Box mt={1}>
+                <Box mt={isLastQuestion(num) ? 0 : 1} textAlign={'center'}>
                     <Typography color={'error'} align={'center'} hidden={!hasError}>
                         Выберите один из вариантов ответа
                     </Typography>
                     <GoButton variant={'contained'}
                               size={'large'}
                               fullWidth
-                              sx={{mt: 1}}
+                              sx={{mt: isLastQuestion(num) ? 0 : 1}}
                               onClick={goNext}
                     >
-                        {num === maxNum ? 'Завершить' : 'Следующий вопрос'}
+                        {isLastQuestion(num) ? 'Завершить' : 'Следующий вопрос'}
                     </GoButton>
                 </Box>
             </Stack>
+        </Container>
+    )
+}
+
+const Question = ({num, question}) => {
+    const [answerId, setAnswerId] = useState(-1);
+    const [hasError, setHasError] = useState(false);
+
+    num = parseInt(num);
+
+    let navigate = useNavigate();
+
+    const goNext = () => {
+        if (answerId === -1 && !isLastQuestion(num)) {
+            setHasError(true);
+            return;
+        }
+        sendData(answerId);
+        setAnswerId(-1);
+        if (isLastQuestion(num)) {
+            console.log('FINISH');
+            navigate('/result', {replace: true});
+        } else {
+            console.log('NEXT');
+            navigate(`/test/${num + 1}`, {replace: true});
+        }
+    };
+
+    const handleChange = (event, value) => {
+        setHasError(false);
+        setAnswerId(value);
+    }
+
+    return (
+        <Container maxWidth={'lg'} sx={{mt: 1}}>
+            <Grid container justifyContent={'space-between'} alignItems={'center'}>
+                <Grid item xs={2}>
+                    <Typography variant={'h2'} fontWeight={500}>
+                        {num}/{maxNum}
+                    </Typography>
+                </Grid>
+                <Grid item xs={10}>
+                    <Typography variant={'body1'} align={'left'} style={{whiteSpace: 'pre-line'}}>
+                        {question.legend}
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container mt={0} spacing={4} justifyContent={'space-between'} alignItems={'center'}>
+                <Grid item xs={6} md={isWideQuestion(num) || isLastQuestion(num) ? 12 : null} my={0}>
+                    <img
+                        src={city}
+                        alt={'Comics'}
+                        style={{
+                            height: 'auto',
+                            boxSizing: 'border-box',
+                            width: '100%',
+                            aspectRatio: question.ratio,
+                            borderWidth: 4,
+                            borderStyle: 'solid',
+                            borderImage: 'linear-gradient(180deg, rgba(244,64,148,1) 0%, rgba(85,74,218,1) 100%)',
+                            borderImageSlice: 1,
+                        }}/>
+                </Grid>
+                <Grid item xs={6} md={isWideQuestion(num) || isLastQuestion(num) ? 12 : null} my={0}>
+                    <Typography variant={'h6'} fontWeight={600}>
+                        {question.question}
+                    </Typography>
+                    <RadioGroup
+                        defaultValue={-1}
+                        onChange={handleChange}
+                    >
+                        <Stack mt={1} spacing={1}>
+                            <div/>
+                            {
+                                question.answers?.map((ans) => {
+                                    return ans.id === answerId ?
+                                        <SelectedRadioControlLabel value={ans.id} label={ans.text} key={ans.id}/>
+                                        :
+                                        <RadioControlLabel value={ans.id} label={ans.text} key={ans.id}/>
+                                })
+                            }
+                        </Stack>
+                    </RadioGroup>
+                    <Box mt={isLastQuestion(num) ? 0 : 1} textAlign={'center'}>
+                        <Typography color={'error'} align={'center'} hidden={!hasError}>
+                            Выберите один из вариантов ответа
+                        </Typography>
+                        <GoButton variant={'contained'}
+                                  size={'large'}
+                                  fullWidth={num !== 3}
+                                  sx={{mt: isLastQuestion(num) ? 0 : 1, px: 20}}
+                                  onClick={goNext}
+                        >
+                            {isLastQuestion(num) ? 'Завершить' : 'Следующий вопрос'}
+                        </GoButton>
+                    </Box>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
